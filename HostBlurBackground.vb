49d16ced478b6a7b494610dd2d08597b51ce7b2c
Nukepayload2 <1939357182@qq.com>
2017-05-15 23:36:14 +0800

    磨砂玻璃背景

---
+++ HostBlurBackground.vb
@@ -0,0 +1,71 @@
+﻿Imports Windows.UI
+Imports Windows.UI.Composition
+Imports Windows.UI.Xaml.Hosting
+
+Public Class HostBlurBackground
+    Inherits DependencyObject
+
+    Shared sprite As SpriteVisual
+    Shared WithEvents HostBackdrop As Panel
+
+    Public Shared Function GetIsEnabled(element As DependencyObject) As Boolean
+        If element Is Nothing Then
+            Throw New ArgumentNullException("element")
+        End If
+        Return element.GetValue(IsEnabledProperty)
+    End Function
+
+    Public Shared Sub SetIsEnabled(element As DependencyObject, value As Boolean)
+        If element Is Nothing Then
+            Throw New ArgumentNullException("element")
+        End If
+        element.SetValue(IsEnabledProperty, value)
+    End Sub
+
+    Public Shared ReadOnly IsEnabledProperty As _
+                           DependencyProperty = DependencyProperty.RegisterAttached("IsEnabled",
+                           GetType(Boolean), GetType(Panel),
+                           New PropertyMetadata(False,
+                                                Sub(s, e)
+                                                    HostBackdrop = s
+                                                    If sprite Is Nothing Then
+                                                        sprite = HostBackdropBlurHelper.CreateBackdropSpriteVisual(HostBackdrop)
+                                                    End If
+                                                    If Not e.NewValue Then
+                                                        ElementCompositionPreview.SetElementChildVisual(HostBackdrop, Nothing)
+                                                    End If
+                                                End Sub))
+
+    Public Shared Function GetThemeColor(ByVal element As DependencyObject) As Color
+        If element Is Nothing Then
+            Throw New ArgumentNullException("element")
+        End If
+
+        Return element.GetValue(ThemeColorProperty)
+    End Function
+
+    Public Shared Sub SetThemeColor(ByVal element As DependencyObject, ByVal value As Color)
+        If element Is Nothing Then
+            Throw New ArgumentNullException("element")
+        End If
+
+        element.SetValue(ThemeColorProperty, value)
+    End Sub
+
+    Private Shared Sub HostBackdrop_SizeChanged(sender As Object, e As SizeChangedEventArgs) Handles HostBackdrop.SizeChanged
+        If sprite IsNot Nothing Then
+            sprite.Size = New Numerics.Vector2(HostBackdrop.ActualWidth, HostBackdrop.ActualHeight)
+        End If
+    End Sub
+
+    Public Shared ReadOnly ThemeColorProperty As _
+                           DependencyProperty = DependencyProperty.RegisterAttached("ThemeColor",
+                           GetType(Color), GetType(Panel),
+                           New PropertyMetadata(Nothing,
+                                                Sub(s, e)
+                                                    Dim color = If(e.NewValue, Colors.Transparent)
+                                                    HostBackdropBlurHelper.SetTitleBarColor(color)
+                                                    DirectCast(s, Panel).Background = New SolidColorBrush(color)
+                                                End Sub))
+
+End Class
---
+++ MainPage.xaml.vb
@@ -0,0 +1,21 @@
+﻿' https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+''' <summary>
+''' 可用于自身或导航至 Frame 内部的空白页。
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub BtnFullScreen_Click(sender As Object, e As RoutedEventArgs) Handles BtnFullScreen.Click
+        With ApplicationView.GetForCurrentView
+            If .IsFullScreenMode Then
+                .ExitFullScreenMode()
+                IcnFullScreen.Glyph = ChrW(Symbol.FullScreen)
+            Else
+                .TryEnterFullScreenMode()
+                IcnFullScreen.Glyph = ChrW(Symbol.BackToWindow)
+            End If
+        End With
+    End Sub
+
+End Class
